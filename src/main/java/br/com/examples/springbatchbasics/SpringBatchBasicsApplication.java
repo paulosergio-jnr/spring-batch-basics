bc855929ce9e2da3b3f73d27824e2a634b874fc6
Paulo Sergio Pereira Junior <paulosergio.jnr@gmail.com>
2020-05-31 12:24:27 -0300

    Initial commit

---
+++ src/main/java/br/com/examples/springbatchbasics/SpringBatchBasicsApplication.java
@@ -0,0 +1,13 @@
+package br.com.examples.springbatchbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBasicsApplication.class, args);
+	}
+
+}
